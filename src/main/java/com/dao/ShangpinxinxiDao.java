bf49578ec8dd1552d384cb933a7b8c587cdcf28f
yulinyu <2508000341@QQ.COM>
2025-01-28 17:02:54 +0800

    1

---
+++ src/main/java/com/dao/ShangpinxinxiDao.java
@@ -0,0 +1,36 @@
+package com.dao;
+
+import com.entity.ShangpinxinxiEntity;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+import java.util.List;
+import java.util.Map;
+import com.baomidou.mybatisplus.mapper.Wrapper;
+import com.baomidou.mybatisplus.plugins.pagination.Pagination;
+
+import com.utils.PageUtils;
+import org.apache.ibatis.annotations.Param;
+import com.entity.vo.ShangpinxinxiVO;
+import com.entity.view.ShangpinxinxiView;
+
+
+/**
+ * 商品信息
+ * 
+ * @author 
+ * @email 
+ * @date 2021-01-08 22:38:58
+ */
+public interface ShangpinxinxiDao extends BaseMapper<ShangpinxinxiEntity> {
+	
+	List<ShangpinxinxiVO> selectListVO(@Param("ew") Wrapper<ShangpinxinxiEntity> wrapper);
+	
+	ShangpinxinxiVO selectVO(@Param("ew") Wrapper<ShangpinxinxiEntity> wrapper);
+	
+	List<ShangpinxinxiView> selectListView(@Param("ew") Wrapper<ShangpinxinxiEntity> wrapper);
+
+	List<ShangpinxinxiView> selectListView(Pagination page,@Param("ew") Wrapper<ShangpinxinxiEntity> wrapper);
+	
+	ShangpinxinxiView selectView(@Param("ew") Wrapper<ShangpinxinxiEntity> wrapper);
+
+    PageUtils baseMapper(@Param("params") Map<String, Object> params);
+}
